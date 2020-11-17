a2d1982d9c1eaf81a7f55a7f523c6a4fff7a9abb
sjwakeman <swakeman@ca.rr.com>
2020-11-16 21:20:38 -0800

    Removed console.log from getContacts function. Added .then(contacts => { use forEach to get nested JSON data, const contactMarkup to display contact.id, contact.attributes.name, contact.attributes.date, contact.attributes.category, contact.attributes.location, contact.attributes.occurrence, contact.attributes.individual.name, button data-id contact.id edit button and document.querySelector contact-container innerHTML contactMarkup in rails.js file. Added div id contact-container /div inside body tag in rails.html file.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
 const endPoint = "http://localhost:3000/api/v1/contacts";
-
+ 
 document.addEventListener('DOMContentLoaded', () => {
-    // Fetch and load contacts
     getContacts()
 });
-
+ 
 function getContacts() {
     fetch(endPoint)
     .then(res => res.json())
-    .then(contacts => console.log(contacts));
+    .then(contacts => {
+ 
+        // remember our JSON data is a bit nested due to our serializer
+        contacts.data.forEach(contact => {
+        // double check how your data is nested in the console so you can successfully access the attributes of each individual object
+        const contactMarkup = `
+          <div data-id=${contact.id}>
+            <h3>${contact.attributes.name}</h3>
+            <p>${contact.attributes.date}</p>
+            <p>${contact.attributes.category}</p>
+            <p>${contact.attributes.location}</p>
+            <p>${contact.attributes.occurrence}</p>
+            <p>${contact.attributes.individual.name}</p>
+            <button data-id=${contact.id}>edit</button>
+          </div>
+          <br><br>`;
+ 
+          document.querySelector('#contact-container').innerHTML += contactMarkup
+        })
+    }) 
 }
+
 
