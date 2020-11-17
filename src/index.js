36c71d927861e609ff47470e9cc95b0a6fa497ae
sjwakeman <swakeman@ca.rr.com>
2020-11-16 21:02:21 -0800

    Removed console.log from eventListener and changed to getContacts function call. Created getContacts function fetch(endpoint)  .then(res => res.json()) >     .then(contacts => console.log(contacts)); and confirmed displayed contacts data in console tab.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+const endPoint = "http://localhost:3000/api/v1/contacts";
+
 document.addEventListener('DOMContentLoaded', () => {
-    alert('LOADED');
+    // Fetch and load contacts
+    getContacts()
 });
+
+function getContacts() {
+    fetch(endPoint)
+    .then(res => res.json())
+    .then(contacts => console.log(contacts));
+}
+
