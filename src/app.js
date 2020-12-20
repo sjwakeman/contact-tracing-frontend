b1c22a6c47a31d01a3862a5a4a0cce5c93885a1e
sjwakeman <swakeman@ca.rr.com>
2020-12-20 10:13:38 -0800

    Lost app.js file upon git pull. Had to follow notes to reconstruct app.js file.

--- src/app.js
+++ src/app.js
@@ -1,46 +1,51 @@
 class App {
     constructor() {
-        this.adapter = new Adapter();
-        // this.handleEditClick = this.handleEditClick.bind(this) // FUTURE CODE
+        this.adapter = new Adapter();  
+        this.handleEditClick = this.handleEditClick.bind(this); 
+        this.handleFormSubmit = this.handleFormSubmit.bind(this);                            
+        this.createContacts = this.createContacts.bind(this);                               
+        this.addContacts = this.addContacts.bind(this);   
     }
     attachEventListeners() {
-        document.querySelector(`#contact-container`).addEventListener("click", (e) => {
-        // console.log("CLICKED"); //WORKED
-        e.preventDefault();
-        const id = e.target.dataset.id;
-    // debugger
-        const contact = Contact.findById(id);
-        // debugger
-        // EDIT FUNCTIONALITY HERE
-        document.querySelector('#update').innerHTML = contact.updateContactCard(); 
-    });
+        document.querySelector(`#contact-container`).addEventListener("click", this.handleEditClick);
+        document.querySelector(`#update`).addEventListener('submit', this.handleFormSubmit); 
+    }
 
-    document.querySelector(`#update`).addEventListener('submit', e => {
+    // The previous functionality is broken up into two different methods for future reuse
+    createContacts(contacts) {
+        contacts.data.forEach(contact => {
+            new Contact(contact, contact.attributes);
+        });
+        this.addContacts();
+    }
+    addContacts() {
+        document.querySelector('#contact-container').innerHTML = ``;
+        Contact.all.forEach(
+            contact => (document.querySelector(`#contact-container`).innerHTML += contact.renderContactCard())
+        )
+    }
+ 
+    handleFormSubmit(e) {
         e.preventDefault();
         const id = e.target.dataset.id;  
-        const contact = Contact.findById(id);
-        // const name = this.name;
-        const name = contact.name;
-        // const date = this.date;
-        const date = contact.date;
-        // const category = this.category;
-        const category = contact.category;
-        // const location = this.location;
-        const location = contact.location;
-        // const occurrence = this.occurrence;
+        const contact = Contact.findById(id); 
+        const name = contact.name; 
+        const date = contact.date;  
+        const category = contact.category; 
+        const location = contact.location; 
         const occurrence = contact.occurrence;
-        // const individual_name = this.individual_name; // ORIGINAL
-        // const individual_id = this.individual_id;
         const individual_name = contact.individual_id; 
-                
-        const jsonBody = {name, date, category, location, occurrence, individual_name}; // ORIGINAL
-        // const jsonBody = {name, date, category, location, occurrence, individual_id};
-        // debugger
-
-        this.adapter.updateContactCard(id, jsonBody).then(updatedContact => console.log(updatedContact)); 
-        // this.adapter.updatedContact(id, jsonBody).then(updatedContact => console.log(updatedContact));
-    })
-
+        
+        const jsonBody = {name, date, category, location, occurrence, individual_name};
+        this.adapter.updateContactCard(id, jsonBody).then(updatedContact => console.log(updatedContact));
+    }
+ 
+    handleEditClick(e) {
+        const id = e.target.dataset.id;  
+        const contact = Contact.findById(id); 
+        document.querySelector(`#update`).innerHTML = contact.updateContactCard();
     }
 }
+
+
 
